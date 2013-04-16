5d688ea6e111d45c55a32176a5ff9cef6d8ea0e0
misterjt <misterjt@mail.ru>
2013-04-16 20:40:22 +0400

    Update SMSPilot.js
    Класс для отправки СМС.

--- SMSPilot.js
+++ SMSPilot.js
@@ -11,16 +11,24 @@ SMSPilot.prototype.set=function(name,val){
 	return this.storage[name];
 }
 /* <Short-hand> */
+SMSPilot.prototype.id=function(id){this.set("id",id)}
 SMSPilot.prototype.to=function(to){this.set("to",to)}
 SMSPilot.prototype.text=function(text){this.set("text",text)}
 SMSPilot.prototype.from=function(from){this.set("from",from)}
+SMSPilot.prototype.send_datetime=function(send_datetime){this.set("send_datetime",send_datetime)}
 /* </Short-hand> */
 SMSPilot.prototype.send=function(){
 	var self=this;
 	var data=JSON.stringify({
 		apikey: this.apikey,
 		send: [
-			{from:this.set("from"), to:this.set("to"), text:this.set("text")}
+			{
+				id:this.set("id"),
+				from:this.set("from"), 
+				to:this.set("to"), 
+				text:this.set("text"),
+				send_datetime:this.set("send_datetime")
+			}
 		]
 	});
 	var request=http.request({
@@ -34,14 +42,13 @@ SMSPilot.prototype.send=function(){
 			}
 		}, 
 		function(res) {
-			self.emit("sent");
 			res.setEncoding('utf8');
 			res.on('data', function(chunk){
 				var response=JSON.parse(chunk);
 				if(typeof response.error!="undefined")
 					self.emit("error",response);
 				else
-					self.emit("ready",response);
+					self.emit("sent",response);
 			});
 		}
 	);
